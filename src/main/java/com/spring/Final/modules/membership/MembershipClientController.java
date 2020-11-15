18c7e4e9513b5cec4f6f59374dfc1ce6de221063
Lam Hoang <hieuruoixanhxalo@gmail.com>
2020-11-15 23:24:35 +0700

    membership ui

--- src/main/java/com/spring/Final/modules/membership/MembershipClientController.java
+++ src/main/java/com/spring/Final/modules/membership/MembershipClientController.java
@@ -3,7 +3,9 @@ package com.spring.Final.modules.membership;
 import com.spring.Final.core.infrastructure.ApiController;
 import com.spring.Final.core.infrastructure.ApiResult;
 import com.spring.Final.modules.membership.projections.MembershipDetail;
+import com.spring.Final.modules.shared.enums.membership_duration.MembershipDuration;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,8 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
-@CrossOrigin
+@Controller
 @RequestMapping("/memberships")
 public class MembershipClientController extends ApiController {
 
@@ -24,8 +28,11 @@ public class MembershipClientController extends ApiController {
 
     @GetMapping("")
     public String list(Model modelView) {
-//        List<MembershipDetail> data = service.list();
-
+        List<MembershipDetail> data = service.list();
+        List<MembershipDetail> listMonthly = data.stream().filter(x->x.getDuration() == MembershipDuration.MONTHLY).collect(Collectors.toList());
+        List<MembershipDetail> listYearly = data.stream().filter(x->x.getDuration() == MembershipDuration.YEARLY).collect(Collectors.toList());
+        modelView.addAttribute("listMonthly", listMonthly);
+        modelView.addAttribute("listYearly", listYearly);
         return "client/modules/memberships/membership";
     }
 }
