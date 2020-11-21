8f70a8fa604b97b9c98bcfd78497ae0ebf064b96
Lam Hoang <hieuruoixanhxalo@gmail.com>
2020-11-21 23:54:46 +0700

    manage-proposals

---
+++ src/main/java/com/spring/Final/modules/job_proposal/projections/JobProposalEmployee.java
@@ -0,0 +1,23 @@
+package com.spring.Final.modules.job_proposal.projections;
+
+import com.spring.Final.modules.employer.EmployerEntity;
+import com.spring.Final.modules.jobs.projections.JobDetail;
+import com.spring.Final.modules.shared.enums.job_proposal_status.JobProposalStatus;
+import com.spring.Final.modules.shared.enums.job_proposal_type.JobProposalType;
+import com.spring.Final.modules.shared.enums.job_status.JobStatus;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+public class JobProposalEmployee {
+    private int id;
+    private JobDetail job;
+    private JobProposalType type;
+    private JobProposalStatus status;
+    private Date startedAt;
+    private Date endedAt;
+    private Date createdAt;
+
+}
