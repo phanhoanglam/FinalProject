e06031186466841ce915827c642c553b4055c1ea
Lam Hoang <hieuruoixanhxalo@gmail.com>
2020-11-25 00:03:46 +0700

    employers manage

--- myadmin/app/routes.js
+++ myadmin/app/routes.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const pageRoutes = require('./page/routes');
 const jobTypeRoutes = require('./job_type/routes');
 const jobCategoryRoutes = require('./job_category/routes');
+const employerRoutes = require('./employer/routes');
 
 router.use('/', pageRoutes);
 router.use('/job-types', jobTypeRoutes);
 router.use('/job-categories', jobCategoryRoutes);
+router.use('/employers', employerRoutes);
 
 module.exports = router;
